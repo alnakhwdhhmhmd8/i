ce172db8ba9a0d030bcbb9073f3fae7ebd1910f4
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2026-01-19 04:51:04 +0300

    Add config.py via Telegram Bot

---
+++ m8n/config.py
@@ -0,0 +1,40 @@
+# Created By @xl444
+# Copyright By watan
+
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+que = {}
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "BAFF9UIAWkq2jwTejGemt_8MzR4E4tTu-QxKjR--fdTvyiO6t21974mIrGwl62kUq-w3D9WX0dDkpKNISlvid5g2R4Eb_OjRjkZAitzzVgdSJcK1sIjqjPa4I88boUEWYKHYfRVEgtW9DaV0vIN3dMp0m8ar3xlDkeelp0BocYyHD433l0SJP5jrSoJYl2Uw7munvjHljM6qIncgPYmhigQYpKJkjOyLYl20TCHDBhZtjD9WNT5r_ZqWg93OT80AFlqIekMqRfPV-HkEuegx_zAs1_Lqrqeg_Bytnl7_5nxuq0gsjERPh5WDkTKawF22vSA19jQavHd7gX_FkSiIrZ9aG-3PCwAAAAH8H0OjAA")
+BOT_TOKEN = getenv("BOT_TOKEN", "8211179420:AAGTwuBSJ0HZ8tzxtmvox3GRMxaAmnhngJI")
+BOT_NAME = getenv("BOT_NAME", "bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "Saiadjabot")
+ASSID = int(getenv("ASSID", "7728230165"))
+ASSNAME = getenv("ASSNAME", "wwvvwl")
+ASSUSERNAME = getenv("ASSUSERNAME", "Kasijekg")
+
+BOT_ID = int(getenv("BOT_ID", "7728230165"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/STKR2/LG")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
+API_ID = int(getenv("API_ID", "8934899"))
+API_HASH = getenv("API_HASH", "bf3e98d2c351e4ad06946b4897374a1e")
+OWNER_ID = int(getenv("OWNER_ID", "7728230165"))
+UPDATE = getenv("UPDATE", "xvwwvl")
+SUPPORT = getenv("SUPPORT", "xvwwvl")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "999"))
+CMD_MUSIC = list(getenv("CMD_MUSIC", ". / ! + - @ # $").split())
+BG_IMG = getenv("BG_IMG", "https://te.legra.ph/file/466de30ee0f9383c8e09e.jpg")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+START_PIC = getenv("START_PIC", "https://graph.org/file/8882cbd7cc786826d9ecb.jpg")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "wwvvwl")
+IMG_1 = getenv("IMG_1", "https://graph.org/file/475127193de2444183fd4.jpg")
+IMG_2 = getenv("IMG_2", "https://graph.org/file/8882cbd7cc786826d9ecb.jpg")
+IMG_3 = getenv("IMG_3", "https://graph.org/file/8882cbd7cc786826d9ecb.jpg")
+IMG_4 = getenv("IMG_4", "https://graph.org/file/8882cbd7cc786826d9ecb.jpg")
